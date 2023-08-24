99a5b9ffc54ea2232fecae9f7cdb797f4840e12b
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-08-24 07:03:28 +0530

    add task init

--- src/controller.ts
+++ src/controller.ts
@@ -31,3 +31,59 @@ const getTasks = (req: IncomingMessage, res: ServerResponse) => {
     },
   );
 };
+
+const addTask = (req: IncomingMessage, res: ServerResponse) => {
+  let data = "";
+
+  req.on("data", (chunk) => {
+    data += chunk.toString();
+  });
+
+  req.on("end", () => {
+    let task = JSON.parse(data);
+
+    fs.readFile(path.join(__dirname, "store.json"), "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            success: false,
+            error: err,
+          }),
+        );
+      } else {
+        let tasks: Task[] = JSON.parse(data);
+        let latest_id = tasks.reduce(
+          (max = 0, task: Task) => (task.id > max ? task.id : max),
+          0,
+        );
+        task.id = latest_id + 1;
+        tasks.push(task);
+
+        fs.writeFile(
+          path.join(__dirname, "store.json"),
+          JSON.stringify(tasks),
+          (err) => {
+            if (err) {
+              res.writeHead(500, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({
+                  success: false,
+                  error: err,
+                }),
+              );
+            } else {
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({
+                  success: true,
+                  message: task,
+                }),
+              );
+            }
+          },
+        );
+      }
+    });
+  });
+};
